8a181e219d4ecc9af3d6d23b8367d7f689e198b2
kana.miyamoto <kana.miyamoto@nijibox.co.jp>
2019-09-19 12:42:40 +0900

    to do app   コンポーネント作成
    【Title】コンポーネント作成。

---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class Title extends
+React.Component {
+    render(){
+       return(
+           <h1>Todo App</h1>
+       );
+    }
+}
+export default Title;
